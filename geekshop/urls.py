4de19d9c9d70383658818996f7a2a36eccf8c2be
Anton Mindlin <anton.mindlin@gmail.com>
2022-02-26 16:19:58 +0100

    Lesson_3. Urls mainapp

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import mainapp.views as mainapp
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainapp.index, name='index'),
-    path('products/', mainapp.products, name='products')
+    path('products/', include('mainapp.urls', namespace='products'))
 ]
 
 if settings.DEBUG:
