2f737c16021576439872c6b3c06bf16bbe7f6ab3
Anton Mindlin <anton.mindlin@gmail.com>
2022-02-26 14:59:50 +0100

    Lesson_3. Product and category models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='имя', max_length=128, unique=True)
+    description = models.TextField(verbose_name='описание продукта', blank=True, null=True)
+    image_url = models.ImageField(upload_to='products_images', blank=True)
+    price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
+
+    def __str__(self):
+        return f'{self.name} | {self.category}'
