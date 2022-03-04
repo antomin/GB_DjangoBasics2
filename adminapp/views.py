6482ad9ca122d60bc51c6a8bd97a3e7073172927
Anton Mindlin <anton.mindlin@gmail.com>
2022-03-04 20:46:53 +0100

    Lesson_7. Adminapp category READ

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from authapp.models import ShopUser
+from mainapp.models import ProductCategory
 
 
 def index(request):
@@ -10,19 +11,61 @@ def index(request):
 def user_read(request):
     context = {
         'title': 'Пользователи',
-        'users': ShopUser.objects.all(),
+        'users': ShopUser.objects.all().order_by('-is_active', '-is_staff', 'username'),
     }
 
-    return render(request, 'adminapp/admin-users-read.html', context)
+    return render(request, 'adminapp/users.html', context)
 
 
 def user_create(request):
-    return render(request, 'adminapp/admin-users-create.html')
+    return render(request, 'adminapp/users_create.html')
 
 
 def user_edit(request):
-    return render(request, 'adminapp/admin-users-edit.html')
+    return render(request, 'adminapp/users_edit.html')
 
 
 def user_delete(request, user_pk):
-    return render(request, 'adminapp/admin-users-edit.html')
+    return render(request, 'adminapp/users_edit.html')
+
+
+def category_read(request):
+    context = {
+        'title': 'Категории',
+        'categories': ProductCategory.objects.all().order_by('-is_active', 'name'),
+    }
+
+    return render(request, 'adminapp/categories.html', context)
+
+
+def category_create(request):
+    return render(request, 'adminapp/categories_create.html')
+
+
+def category_edit(request):
+    return render(request, 'adminapp/categories_edit.html')
+
+
+def category_delete(request):
+    return render(request, 'adminapp/categories_delete.html')
+
+
+def product_read(request):
+    context = {
+        'title': 'Категории',
+        'categories': ProductCategory.objects.all().order_by('-is_active', 'name'),
+    }
+
+    return render(request, 'adminapp/products.html', context)
+
+
+def product_create(request):
+    return render(request, 'adminapp/products_create.html')
+
+
+def product_edit(request):
+    return render(request, 'adminapp/products_edit.html')
+
+
+def product_delete(request, user_pk):
+    return render(request, 'adminapp/products_delete.html')
--- mainapp/models.py
+++ mainapp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class ProductCategory(models.Model):
     name = models.CharField(verbose_name='имя', max_length=64, unique=True)
     description = models.TextField(verbose_name='описание', blank=True, null=True)
+    is_active = models.BooleanField(verbose_name='активна', default=True)
 
     def __str__(self):
         return self.name
@@ -16,6 +17,7 @@ class Product(models.Model):
     image_url = models.ImageField(upload_to='products_images', blank=True, verbose_name='изображение')
     price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name='количество на сладе', default=0)
+    is_active = models.BooleanField(verbose_name='активен', default=True)
 
     def __str__(self):
         return f'{self.name} | {self.category}'
