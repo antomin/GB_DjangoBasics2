2ade5e3cf8c8953c4dc41aaebbbcd3d7ed9522b9
Anton Mindlin <anton.mindlin@gmail.com>
2022-03-04 20:11:33 +0100

    Lesson_7. Adminapp user READ

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', adminapp.index, name='index'),
     path('users/view/', adminapp.user_read, name='user_view'),
     path('users/create/', adminapp.user_create, name='user_create'),
-    path('users/update/', adminapp.user_update, name='user_update'),
+    path('users/edit/<int:user_pk>', adminapp.user_edit, name='user_edit'),
     path('users/delete/', adminapp.user_delete, name='user_delete'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render
 
+from authapp.models import ShopUser
+
 
 def index(request):
     return render(request, 'adminapp/admin.html')
 
 
 def user_read(request):
-    return render(request, 'adminapp/admin-users-read.html')
+    context = {
+        'title': 'Пользователи',
+        'users': ShopUser.objects.all(),
+    }
+
+    return render(request, 'adminapp/admin-users-read.html', context)
 
 
 def user_create(request):
     return render(request, 'adminapp/admin-users-create.html')
 
 
-def user_update(request):
-    return render(request, 'adminapp/admin-users-update.html')
+def user_edit(request):
+    return render(request, 'adminapp/admin-users-edit.html')
 
 
-def user_delete(request):
-    return render(request, 'adminapp/admin-users-update.html')
+def user_delete(request, user_pk):
+    return render(request, 'adminapp/admin-users-edit.html')
